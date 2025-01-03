dc94afb368069a7848aac4ea9c1c101fc3518298
Chethants05 <chethants05@gmail.com>
2025-01-03 16:08:50 +0530

    new changes

--- newproject/src/App.jsx
+++ newproject/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Mainhome from './mainHome'
 import UploadPage from './components/uploadpage';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 const App = () => {
   return (
-
+    <BrowserRouter>
     <Router>
     <Routes>
       <Route path="/" element={<Mainhome />} />
       <Route path="/upload" element={<UploadPage />} />
     </Routes>
   </Router>
+  </BrowserRouter>
   );
 };
 
